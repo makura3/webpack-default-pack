da67a55cdbaf27645ea05e4b18128976393b4b90
makura3 <37650810+makura3@users.noreply.github.com>
2018-06-02 23:18:03 +0900

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -2,28 +2,54 @@
 // import ExtractTextPlugin from 'extract-text-webpack-plugin'
 const path = require('path'),
     ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    glob = require("glob");
+    // glob = require("glob"),
+    globule = require('globule');
 
 
-//ejsのコンパイル対象のファイルを抽出する。
-const entryFileList = {};
-glob.sync('./src/components/**/!(_*.ejs)').map(function(file) {
-  entryFileList[file.replace(/\.[a-z]+$/, '')] = file;
-});
+// //ejsのコンパイル対象のファイルを抽出する。
+// const entryFileList = {};
+// glob.sync('./src/components/**/!(_*.ejs)').glob.find('_*.ejs').map(function(file) {
+//     entryFileList[file.replace(/\.[a-z]+$/, '')] = file;
+//   });
+//   //entryFileList[file.replace(/\.[a-z]+$/, '')] = file;
+// //});
 
-console.log('--------');
+// console.log('--------');
 
-//scssのコンパイル対象のファイルを抽出する。
-glob.sync('./src/scss/**/!(_*.scss)').map(function(file) {
-  entryFileList[file.replace(/\.[a-z]+$/, '')] = file;
-  console.log(file);
-});
+// //scssのコンパイル対象のファイルを抽出する。
+// glob.sync('./src/scss/**/!(_*.scss)').map(function(file) {
+//   entryFileList[file.replace(/\.[a-z]+$/, '')] = file;
+//   console.log(file);
+// });
 
-console.log('--------');
-for (key in entryFileList) {
-  console.log('key:' + key + ' value:' + entryFileList[key]);
+// console.log('--------');
+// for (key in entryFileList) {
+//   console.log('key:' + key + ' value:' + entryFileList[key]);
+// }
+// //ディレクトリで切れているものを除外
+
+// ディレクトリの設定
+const opts = {
+  srcDir: path.join(__dirname, 'site'),
+  destDir: path.join(__dirname, 'site/css')
 }
-//ディレクトリで切れているものを除外
+
+const convertExtensions = {
+  ejs: 'ejs',
+  scss: 'scss',
+  js: 'js'
+}
+
+const entryFileList = {}
+Object.keys(convertExtensions).forEach(extension => {
+  globule.find([`**/*.${extension}`, `!**/_*.${extension}`], {cwd: opts.srcDir}).forEach(filename => {
+    console.log(filename);
+    console.log(extension);
+    entryFileList[filename.replace(new RegExp(`.${extension}$`, 'i'), '')] = path.join(opts.srcDir, filename)
+  })
+})
+
+console.log(entryFileList);
 
 
 module.exports = {
@@ -34,7 +60,7 @@ module.exports = {
   // },
   entry: entryFileList,
   output: {
-      path: path.join(__dirname, 'src/css/'),
+      path: path.join(__dirname, 'dist/'),
       filename: '[name].css'
   },
   module: {
